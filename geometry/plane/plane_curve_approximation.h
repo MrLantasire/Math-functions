66689026e24a3ac9f8fd4064b666ec7d657fcb91
Igor Buchenkov (MrLantasire) <ibuchenkov.91@mail.ru>
2022-07-24 15:08:57 +0300

    Added plane curve approximation module

---
+++ geometry/plane/plane_curve_approximation.h
@@ -0,0 +1,10 @@
+#ifndef __PLANE_CURVE_APPROXIMATION_H
+#define __PLANE_CURVE_APPROXIMATION_H
+
+// Аппроксимация набора точек окружностью
+extern float Circle_approximation(float *points, unsigned short points_num, float *x_centr, float *y_centr);
+
+// Аппроксимация набора точек прямой (параметрический вид)
+extern float Line_approximation(float *points, unsigned short points_num, float *x_point, float *y_point, float *x_vector, float *y_vector);
+
+#endif //__PLANE_CURVE_APPROXIMATION_H
