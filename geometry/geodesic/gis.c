9b962f3bb532781aa65034b5597f8be08a737a73
Igor Buchenkov (MrLantasire) <ibuchenkov.91@mail.ru>
2022-08-19 07:48:38 +0300

    Fixed mistakes in gis functions

--- geometry/geodesic/gis.c
+++ geometry/geodesic/gis.c
@@ -30,12 +30,15 @@ float Get_distance_from_geopoints(float first_latitude, float first_longitude, f
 ************************************************************/
 float Get_direction_from_geopoints(float first_latitude, float first_longitude, float second_latitude, float second_longitude)
 {
-    double argument = 0.0;
+    double numenator = 0.0;
+    double denominator = 0.0;
 
-    argument = cos( GRAD_TO_RAD(first_latitude) ) * sin( GRAD_TO_RAD(second_latitude) ) 
-    - sin( GRAD_TO_RAD(first_latitude) ) * cos( GRAD_TO_RAD(second_latitude) ) * cos( GRAD_TO_RAD(first_longitude - second_longitude) );
+    numenator = sin(GRAD_TO_RAD(second_longitude - first_longitude)) * cos( GRAD_TO_RAD(second_latitude));
 
-    return ((float) RAD_TO_GRAD(atan2(sin(GRAD_TO_RAD(second_longitude - first_longitude)) * cos(second_latitude), argument)));
+    denominator = cos( GRAD_TO_RAD(first_latitude) ) * sin( GRAD_TO_RAD(second_latitude) );
+    denominator -= sin( GRAD_TO_RAD(first_latitude) ) * cos( GRAD_TO_RAD(second_latitude) ) * cos( GRAD_TO_RAD(second_longitude - first_longitude) );
+
+    return ((float) RAD_TO_GRAD(atan2(numenator, denominator)));
 }
 
 /***********************************************************
@@ -58,7 +61,7 @@ void Get_geopoint_to_direction(float latitude, float longitude, float direction,
     *out_latitude = RAD_TO_GRAD( asin(argument) );
 
     argument = ( cos( GRAD_TO_RAD(latitude) ) * cos( distance / radius ) 
-    + sin( GRAD_TO_RAD(latitude) ) * sin( distance / radius ) * cos( GRAD_TO_RAD(direction) ) );
+    - sin( GRAD_TO_RAD(latitude) ) * sin( distance / radius ) * cos( GRAD_TO_RAD(direction) ) );
 
     *out_longitude = longitude + RAD_TO_GRAD( atan2( (sin( distance / radius ) * sin( GRAD_TO_RAD(direction) )), argument ) ) ;
 }
